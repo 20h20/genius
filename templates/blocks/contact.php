5e917d27dad4b69dfdde3438a5d822374900c859
JulienB <brochard.ju@gmail.com>
2024-10-29 10:01:14 +0100

    add picture to contact component

--- templates/blocks/contact.php
+++ templates/blocks/contact.php
@@ -1,9 +1,11 @@
 <?php
 	$title	= get_sub_field('contact_title');
 	$chapo	= get_sub_field('contact_content');
+	$picture	= get_sub_field('contact_picture');
+	$cover	= get_sub_field('contact_cover');
 ?>
 <section class="cbo-contact">
-	<div class="contact-inner cbo-container container--small">
+	<div class="contact-inner cbo-container container--medium">
 
 		<?php if($title): ?>
 			<div class="contact-title cbo-title-2 slide-up" itemprop="headline">
@@ -18,7 +20,18 @@
 		<?php endif; ?>
 
 		<div class="contact-content">
-			<div class="cbo-form slide-up">
+			<div class="content-picture <?php if($cover == 1): ?>cbo-picture-cover<?php endif; ?> <?php if($cover == 0): ?>cbo-picture-contain<?php endif; ?> slide-up">
+				<img
+					decoding="async"
+					src="<?php echo $picture['sizes']['small']; ?>"
+					srcset="<?php echo $picture['sizes']['small']; ?> 320w, <?php echo $picture['sizes']['xlarge']; ?> 768w, <?php echo $picture['sizes']['xlarge']; ?> 1024w"
+					alt="<?php echo $picture['alt']; ?>" sizes="100vw"
+					loading="lazy"
+					width="768" height="768"
+					itemprop="image"
+				>
+			</div>
+			<div class="content-form cbo-form slide-up">
 				<?php
 					$posts = get_sub_field('contact_form');
 					if( $posts ):
